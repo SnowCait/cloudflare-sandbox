2647a2670a5aad4913f02fa7b546a60ebc499a81
SnowCait <SnowCait@users.noreply.github.com>
2024-01-03 10:47:56 +0900

    URL

--- rx-nostr/src/routes/+page.svelte
+++ rx-nostr/src/routes/+page.svelte
@@ -1,18 +1,4 @@
-<script lang="ts">
-    import {createRxNostr} from 'rx-nostr';
-	import { onMount } from 'svelte';
-    const rxNostr = createRxNostr();
-
-    onMount(async () => {
-        await rxNostr.switchRelays(['wss://yabu.me']);
-    })
-
-    async function switchRelay() {
-        await rxNostr.switchRelays(['wss://relay-jp.nostr.wirednet.jp'])
-    }
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button on:click={switchRelay}>Switch relay</button>
+<pre><code>{JSON.stringify(new URL('wss://localhost/'), null, 2)}</code></pre>
