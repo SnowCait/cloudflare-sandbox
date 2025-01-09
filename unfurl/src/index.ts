53818c3d9d21ffc6871636d28db8b204a277e7b8
SnowCait <SnowCait@users.noreply.github.com>
2025-01-10 01:40:43 +0900

    Fix

--- unfurl/src/index.ts
+++ unfurl/src/index.ts
@@ -4,9 +4,9 @@ export default {
 	async fetch(request): Promise<Response> {
 		const url = new URL(request.url).searchParams.get('url');
 		if (url === null) {
-			return new Response(null, {status: 400})
+			return new Response(null, {status: 400});
 		}
-		const data = await unfurl(url)
+		const data = await unfurl(url);
 		return new Response(JSON.stringify(data), {
 			headers: {
 				'Content-Type': 'application/json'
