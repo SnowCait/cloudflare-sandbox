890470e5826cb3f31151b758e12e5c34adb6abf7
SnowCait <SnowCait@users.noreply.github.com>
2025-01-10 01:34:39 +0900

    unfurl.js

--- unfurl/src/index.ts
+++ unfurl/src/index.ts
@@ -1,18 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { unfurl } from "unfurl.js";
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request): Promise<Response> {
+		const url = new URL(request.url).searchParams.get('url');
+		if (url === null) {
+			return new Response(null, {status: 400})
+		}
+		const data = await unfurl(url)
+		return new Response(JSON.stringify(data), {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
 	},
 } satisfies ExportedHandler<Env>;
